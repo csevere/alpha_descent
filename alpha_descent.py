9d7efed1c0bc51daa0fe1880af8e36bcd6e30ca2
csevere <carla.severe@gmail.com>
2018-04-25 17:12:56 -0400

    loaded images and animation

--- alpha_descent.py
+++ alpha_descent.py
@@ -2,12 +2,15 @@
 
 import pygame
 import random 
+from os import path 
+
+img_dir = path.join(path.dirname(__file__), 'img')
 
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
 
-#COLORS 
+######## COLORS ###############
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -24,12 +27,19 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((50, 40))
-		self.image.fill(GREEN)
+		# self.image = player_img
+		#scale the image
+		self.image = pygame.transform.scale(player_img,(50, 38))
+		self.image.set_colorkey(BLACK)
 		self.rect = self.image.get_rect()
+		#giving the sprit a radius / know how big a circle to look at 
+		self.radius = 20
+		#draw a circle on top of the image
+		# pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
 		self.rect.centerx = WIDTH/2
 		self.rect.bottom = HEIGHT - 10
 		self.speedx = 0
+
 
 	def update(self):
 		#default speed should be 0
@@ -53,15 +63,39 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((30, 40))
-		self.image.fill(RED)
+		self.image_orig = random.choice(meteor_images)
+		self.image_orig.set_colorkey(BLACK)
+		self.image = self.image_orig.copy()
 		self.rect = self.image.get_rect()
+		# find the width of rectangle / 2
+		self.radius = int(self.rect.width * .85/ 2)
+		# pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
 		self.rect.x = random.randrange(WIDTH - self.rect.width)
-		self.rect.y = random.randrange(-100, -40)
+		self.rect.y = random.randrange(-150, -100)
 		self.speedy = random.randrange(1, 8)
 		self.speedx = random.randrange(-3, 3)
+		#how far in degree sprite should rotate
+		self.rot = 0
+		#have mod rotate in diff directions / how fast it rotates
+		self.rot_speed = random.randrange(-8, 8)
+		#grabs number of ticks since clock started 
+		self.last_update = pygame.time.get_ticks()
+
+	def rotate(self):
+		now = pygame.time.get_ticks()
+		if now - self.last_update > 50:
+			self.last_update = now
+			#keep track of rotation, making it loop around
+			self.rot = (self.rot + self.rot_speed) % 360
+			new_image = pygame.transform.rotate(self.image_orig, self.rot)
+			#change rectange shape and size and keep the sprite centered when rotating 
+			old_center = self.rect.center
+			self.image = new_image
+			self.rect = self.image.get_rect()
+			self.rect.center = old_center 
 
 	def update(self):
+		self.rotate()
 		#makes it move downward 
 		self.rect.x += self.speedx 
 		self.rect.y += self.speedy
@@ -74,8 +108,8 @@ class Bullet(pygame.sprite.Sprite):
   	#tell bullet to spawn at particular loc according to player
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((10,20))
-		self.image.fill(YELLOW)
+		self.image = laser_img
+		self.image.set_colorkey(BLACK)
 		self.rect = self.image.get_rect()
 		self.rect.bottom = y
 		self.rect.centerx = x
@@ -87,8 +121,21 @@ class Bullet(pygame.sprite.Sprite):
 		if self.rect.bottom < 0:
 			self.kill()
 
-		
-  		  
+
+############ LOADING GAME GRAPHICS ############
+background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
+background_rect = background.get_rect()	
+player_img = pygame.image.load(path.join(img_dir, "playership_blue.png")).convert()
+laser_img = pygame.image.load(path.join(img_dir, "laser_green.png")).convert()
+meteor_images = []
+meteor_list = [	'meteor_big1.png', 'meteor_big2.png', 'meteor_big3.png', 'meteor_big4.png', 
+				'meteor_med1.png', 'meteor_med3.png', 'meteor_small1.png', 'meteor_small2.png', 'meteor_tiny1.png', 
+				'meteor_tiny2.png']
+for img in meteor_list:
+  	meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+
+
+########### CREATE SPRITES ###################  		  
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -101,7 +148,7 @@ for i in range(8):
 	mobs.add(m)
 
 
-#GAME LOOP 
+############## GAME LOOP ######################
 running = True
 while running:
 	clock.tick(FPS)
@@ -114,7 +161,7 @@ while running:
 			 
 
 
-	#update
+	#UPDATE 
 	all_sprites.update()
 
 	#check for bullet/mob collision / true for both so both get deleted
@@ -126,15 +173,16 @@ while running:
 		mobs.add(m)
 
 
-	#check for mob/player collision
-	hits = pygame.sprite.spritecollide(player, mobs, False)
+	#check for mob/player collision/ circle specifies type of collision
+	hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
 	if hits:
   		#game over 
   		running = False 
 
 
-	#render
+	#RENDER
 	screen.fill(BLACK)
+	screen.blit(background, background_rect)
 	all_sprites.draw(screen)
 	pygame.display.flip()
 
